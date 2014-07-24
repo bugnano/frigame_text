2159c0c9ca43a3650031c6663d57c027cafebbf2
fri <devnull@localhost>
2014-07-24 09:34:37 +0200

    First commit

---
+++ frigame.text.canvas.js
@@ -0,0 +1,406 @@
+/*global friGame */
+/*jslint white: true, browser: true */
+
+// Copyright (c) 2011-2014 Franco Bugnano
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+// Uses ideas and APIs inspired by:
+// gameQuery Copyright (c) 2008 Selim Arsever (gamequery.onaluf.org), licensed under the MIT
+
+(function (fg) {
+	'use strict';
+
+	fg.extend(fg.PGradient, {
+		setFillStyle: function (ctx, group) {
+			var
+				width = group.width,
+				height = group.height,
+				dimension
+			;
+
+			if (this.fillStyle) {
+				// Solid color
+				ctx.fillStyle = this.fillStyle;
+			} else {
+				// Gradient
+				if (this.type === fg.GRADIENT_HORIZONTAL) {
+					dimension = width;
+				} else {
+					dimension = height;
+				}
+
+				ctx.fillStyle = this.gradients[dimension].fillStyle;
+			}
+		},
+
+		setStrokeStyle: function (ctx, group) {
+			var
+				width = group.width,
+				height = group.height,
+				dimension
+			;
+
+			if (this.fillStyle) {
+				// Solid color
+				ctx.strokeStyle = this.fillStyle;
+			} else {
+				// Gradient
+				if (this.type === fg.GRADIENT_HORIZONTAL) {
+					dimension = width;
+				} else {
+					dimension = height;
+				}
+
+				ctx.strokeStyle = this.gradients[dimension].fillStyle;
+			}
+		}
+	});
+
+	// ******************************************************************** //
+	// ******************************************************************** //
+	// ******************************************************************** //
+	// ******************************************************************** //
+	// ******************************************************************** //
+
+	fg.PFont = {
+		init: function (options) {
+			var
+				my_options,
+				new_options = options || {}
+			;
+
+			if (this.options) {
+				my_options = this.options;
+			} else {
+				my_options = {};
+				this.options = my_options;
+			}
+
+			// Set default options
+			fg.extend(my_options, {
+				// Public options
+				style: 'normal',		// normal, italic, oblique
+				weight: 'normal',		// normal, bold, bolder, lighter, 100, 200, 300, 400, 500, 600, 700, 800, 900
+				size: 10,				// px
+				family: 'sans-serif'
+
+				// Implementation details
+			});
+
+			// TO DO -- The line-height property should be calculated here in order to be consistent with an eventual DOM text backend
+
+			new_options = fg.extend(my_options, fg.pick(new_options, [
+				'style',
+				'weight',
+				'size',
+				'family'
+			]));
+
+			fg.extend(this, fg.pick(my_options, [
+				'style',
+				'weight',
+				'size',
+				'family'
+			]));
+
+			this.CSSString = [my_options.style, ' ', String(my_options.weight), ' ', String(my_options.size), 'px ', my_options.family].join('');
+		}
+
+		// Public functions
+
+		// Implementation details
+	};
+
+	fg.Font = fg.Maker(fg.PFont);
+
+	fg.resourceManager.addFont = function (name) {
+		var
+			args = Array.prototype.slice.call(arguments, 1),
+			font = fg.Font.apply(this, args)
+		;
+
+		font.name = name;
+
+		return fg.resourceManager.addResource(name, font);
+	};
+
+	// ******************************************************************** //
+	// ******************************************************************** //
+	// ******************************************************************** //
+	// ******************************************************************** //
+	// ******************************************************************** //
+
+	fg.PText = Object.create(fg.PBaseSprite);
+	fg.extend(fg.PText, {
+		init: function (name, options, parent) {
+			var
+				my_options,
+				new_options = options || {}
+			;
+
+			if (this.options) {
+				my_options = this.options;
+			} else {
+				my_options = {};
+				this.options = my_options;
+			}
+
+			// Set default options
+			fg.extend(my_options, {
+				// Public options
+				text: null,
+				font: null,
+				fillColor: null,
+				strokeColor: null,
+				strokeWidth: 2
+
+				// Implementation details
+			});
+
+			fg.PBaseSprite.init.apply(this, arguments);
+
+			this.old_options = {};
+
+			// If the text has not been defined, force
+			// the text to null in order to resize and move
+			// the sprite inside setText
+			if (new_options.text === undefined) {
+				new_options.text = null;
+			}
+
+			this.setText(new_options);
+		},
+
+		// Public functions
+
+		remove: function () {
+			var
+				options = this.options,
+				old_options = this.old_options,
+				fill_color = options.fillColor,
+				stroke_color = options.strokeColor,
+				old_fill_color = old_options.fillColor,
+				old_stroke_color = old_options.strokeColor
+			;
+
+			if (old_fill_color && old_fill_color.removeGroup) {
+				old_fill_color.removeGroup(this);
+			}
+
+			if (old_stroke_color && old_stroke_color.removeGroup) {
+				old_stroke_color.removeGroup(this);
+			}
+
+			if (fill_color && fill_color.removeGroup) {
+				fill_color.removeGroup(this);
+			}
+
+			if (stroke_color && stroke_color.removeGroup) {
+				stroke_color.removeGroup(this);
+			}
+
+			fg.PBaseSprite.remove.apply(this, arguments);
+		},
+
+		setText: function (options) {
+			var
+				my_options = this.options,
+				new_options = options || {},
+				text,
+				font,
+				text_redefined = new_options.text !== undefined,
+				font_redefined = new_options.font !== undefined,
+				ctx = fg.ctx
+			;
+
+			if (text_redefined) {
+				my_options.text = new_options.text;
+			}
+
+			if (font_redefined) {
+				my_options.font = fg.r[new_options.font];
+			}
+
+			if (new_options.fillColor !== undefined) {
+				my_options.fillColor = fg.r[new_options.fillColor];
+			}
+
+			if (new_options.strokeColor !== undefined) {
+				my_options.strokeColor = fg.r[new_options.strokeColor];
+			}
+
+			if (new_options.strokeWidth !== undefined) {
+				my_options.strokeWidth = new_options.strokeWidth * 2;
+			}
+
+			if (text_redefined || font_redefined) {
+				text = my_options.text;
+				font = my_options.font;
+				if (text && font) {
+					ctx.save();
+					ctx.font = font.CSSString;
+					new_options.width = ctx.measureText(text).width;
+					new_options.height = font.size;
+					ctx.restore();
+				} else {
+					new_options.width = 0;
+					new_options.height = 0;
+				}
+
+				// Call the resize method with all the options in order to update the position
+				fg.PBaseSprite.resize.call(this, new_options);
+			}
+
+			return this;
+		},
+
+		resize: null,	// Text cannot be explicitly resized
+
+		// Implementation details
+
+		draw: function () {
+			var
+				options = this.options,
+				old_options = this.old_options,
+				left = this.left,
+				top = this.top,
+				width = this.width,
+				height = this.height,
+				text = options.text,
+				font = options.font,
+				fill_color = options.fillColor,
+				stroke_color = options.strokeColor,
+				stroke_width = options.strokeWidth,
+				stroke = stroke_color && stroke_width,
+				old_fill_color = old_options.fillColor,
+				old_stroke_color = old_options.strokeColor,
+				fill_color_changed = fill_color !== old_fill_color,
+				stroke_color_changed = stroke_color !== old_stroke_color,
+				angle = options.angle,
+				scaleh = options.scaleh,
+				scalev = options.scalev,
+				alpha = options.alpha,
+				old_alpha,
+				ctx = fg.ctx
+			;
+
+			if (fg.insidePlayground(this) && text && font && alpha && scaleh && scalev && !options.hidden && (fill_color || stroke)) {
+				if (fill_color_changed || stroke_color_changed) {
+					if (fill_color_changed) {
+						if (old_fill_color && old_fill_color.removeGroup) {
+							old_fill_color.removeGroup(this);
+						}
+
+						if (fill_color && fill_color.addGroup) {
+							fill_color.addGroup(this);
+						}
+
+						old_options.fill_color = fill_color;
+					}
+
+					if (stroke_color_changed) {
+						if (old_stroke_color && old_stroke_color.removeGroup) {
+							old_stroke_color.removeGroup(this);
+						}
+
+						if (stroke_color && stroke_color.addGroup) {
+							stroke_color.addGroup(this);
+						}
+
+						old_options.stroke_color = stroke_color;
+					}
+
+					old_options.width = width;
+					old_options.height = height;
+				} else {
+					if ((width !== old_options.width) || (height !== old_options.height)) {
+						// Reset the colors in order to create new ones with the new width and height
+						if (fill_color) {
+							if (fill_color.removeGroup) {
+								fill_color.removeGroup(this);
+							}
+
+							if (fill_color.addGroup) {
+								fill_color.addGroup(this);
+							}
+						}
+
+						if (stroke_color) {
+							if (stroke_color.removeGroup) {
+								stroke_color.removeGroup(this);
+							}
+
+							if (stroke_color.addGroup) {
+								stroke_color.addGroup(this);
+							}
+						}
+
+						old_options.width = width;
+						old_options.height = height;
+					}
+				}
+
+				ctx.save();
+
+				if (angle || (scaleh !== 1) || (scalev !== 1)) {
+					ctx.translate(this.centerx, this.centery);
+
+					if (angle) {
+						ctx.rotate(angle);
+					}
+
+					if ((scaleh !== 1) || (scalev !== 1)) {
+						ctx.scale(scaleh, scalev);
+					}
+
+					ctx.translate(-this.halfWidth, -this.halfHeight);
+				} else if (left || top) {
+					ctx.translate(left, top);
+				}
+
+				old_alpha = fg.globalAlpha;
+				if (alpha !== 1) {
+					fg.globalAlpha *= alpha;
+					ctx.globalAlpha = fg.globalAlpha;
+				}
+
+				ctx.font = font.CSSString;
+
+				if (stroke) {
+					stroke_color.setStrokeStyle(ctx, this);
+					ctx.lineWidth = stroke_width;
+					ctx.strokeText(text, 0, 0);
+				}
+
+				if (fill_color) {
+					fill_color.setFillStyle(ctx, this);
+					ctx.fillText(text, 0, 0);
+				}
+
+				ctx.restore();
+
+				fg.globalAlpha = old_alpha;
+			}
+		}
+	});
+
+	fg.Text = fg.Maker(fg.PText);
+}(friGame));
+
